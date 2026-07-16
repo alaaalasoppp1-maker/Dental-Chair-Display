0431fd399624f6bc1759412aee96e23accab6856
alaaalasoppp1-maker <alaa.alasoppp1@gmail.com>
2026-07-16 04:53:22 +0300

    Update Color.kt

--- app/src/main/java/com/dentalchain/display/ui/theme/Color.kt
+++ app/src/main/java/com/dentalchain/display/ui/theme/Color.kt
@@ -2,10 +2,10 @@ package com.dentalchain.display.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
-
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+val DentalBlue = Color(0xFF176EE5)
+val DentalBlueDark = Color(0xFF0B2A43)
+val DentalTeal = Color(0xFF54E5D6)
+val DentalBackground = Color(0xFF061524)
+val DentalSurface = Color(0xFF10344F)
+val DentalText = Color(0xFFF4FAFD)
+val DentalTextSecondary = Color(0xFFC8DCE8)
