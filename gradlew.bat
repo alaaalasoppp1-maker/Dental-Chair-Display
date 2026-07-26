e8c4ca169e44c0ee37a8bd01b1932cc74e0afa8d
alaaalasoppp1-maker <alaa.alasoppp1@gmail.com>
2026-07-26 06:36:46 +0300

    Delete gradlew.bat

--- gradlew.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-where gradle >nul 2>nul
-if %errorlevel%==0 (
-  gradle %*
-  exit /b %errorlevel%
-)
-echo Gradle is not installed. Open the project in Android Studio or use GitHub Actions.
-exit /b 1
